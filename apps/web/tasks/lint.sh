fd83d34b9ff44a04c41ee73c62cc54e78de1b8b9
Damian Nowakowski <damian@airnauts.com>
2024-03-28 19:22:04 +0100

    feat: update linting messages

--- apps/web/tasks/lint.sh
+++ apps/web/tasks/lint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+echo "🛠️  Running ESLint..."
 eslint ./src -c ./.eslintrc.js --format stylish --ext .js,.jsx,.ts,.tsx
+echo "✅ ESLint completed"
+echo "🛠️  Running TypeScript..."
 tsc --noEmit
+echo "✅ TypeScript completed"
